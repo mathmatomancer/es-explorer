b23bd0cdaca7b9158eaabe7557656d495001f111
Brandy Webster <brandy@repairshopr.com>
2016-08-21 11:12:38 -0700

    trying to start showing the list of indices as navigable list

--- web/controllers/index_info_controller.ex
+++ web/controllers/index_info_controller.ex
@@ -2,7 +2,16 @@ defmodule EsExplorer.IndexInfoController do
   use EsExplorer.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+
+    local_host = "localhost:9200"
+    url = "http://#{local_host}/_cat/indices"
+    local_response = HTTPotion.get url
+    # want to parse this, and construct links to the settings page for each detected index
+    list = String.split(local_response.body, "\n")
+#    ul = []
+#    Enum.each(list, fn(x) -> ul = ul ++ ["<li>#{x}</li>"] end)
+
+    render conn, "index.html", list: list
   end
 
   def settings(conn, params) do
--- web/views/index_info_view.ex
+++ web/views/index_info_view.ex
@@ -1,3 +1,7 @@
 defmodule EsExplorer.IndexInfoView do
   use EsExplorer.Web, :view
+
+  def echo_list(list) do
+    list
+  end
 end
